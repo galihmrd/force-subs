ead9c1ce9e46cb8ff8c94cb0dd6c05bb322bd93c
GalihMrd <69835093+galihmrd@users.noreply.github.com>
2024-03-14 10:50:44 +0700

    Update start.py

--- src/modules/start.py
+++ src/modules/start.py
@@ -7,11 +7,9 @@ async def start(_client, _message):
     await _message.reply("Hello i'm alive!")
     Data.write_data(_message.from_user.mention, "user_id")
 
-
 @Client.on_message(filters.forwarded)
 async def get_forwarded_info(_client, _message):
     await _message.reply(f"**Message Forwarded from: `{_message.forward_from_chat.id}`")
-
 
 @Client.on_message(filters.command("add"))
 async def add_channel_autoban(_client, _message):
